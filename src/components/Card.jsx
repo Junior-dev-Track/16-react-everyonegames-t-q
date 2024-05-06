3cb1fd51344d11e1f34ff27166269791643dd244
ovo-thom <thonnard.thomas@gmail.com>
2024-05-06 11:00:28 +0200

    modif games

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,24 @@
 /* eslint-disable react/prop-types */
 const Card = ({ game }) => {
-  // console.log(game);
+  console.log(game);
   return (
-    <li className="card">
+    <li key={game.id} className="card">
       <img src={game.background_image} alt={game.name} />
-      <div className="infos"></div>
+      <div className="game-details">
+        <h2>{game.name}</h2>
+        <p>Plateformes :</p>
+        <ul>
+          {game.parent_platforms.map((platform) => (
+            <li key={platform.platform.id}>{platform.platform.name}</li>
+          ))}
+        </ul>
+      </div>
     </li>
+
+    // <li className="card">
+    //   <img src={game.background_image} alt={game.name} />
+    //   <div className="infos"></div>
+    // </li>
   );
 };
 
